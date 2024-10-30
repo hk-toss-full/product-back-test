c4e88ffb333d9018125db88f6a994834c040087d
tjdanqkr <tjdanqkr@gmail.com>
2024-10-30 15:13:37 +0900

    save

--- src/main/java/com/product/query/controller/ProductController.java
+++ src/main/java/com/product/query/controller/ProductController.java
@@ -4,6 +4,8 @@ import com.product.query.entity.Product;
 import com.product.query.service.KafkaProductServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,6 +17,10 @@ public class ProductController {
     @GetMapping("/api/v1/products")
     public List<Product> getAll(){
         return productService.getProducts();
+    }
+    @PostMapping("/api/v1/products")
+    public void save(@RequestBody Product product){
+        productService.save(product);
     }
 }
 
